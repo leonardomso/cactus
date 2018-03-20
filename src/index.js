83aaa6b2cdebf46f39aee3b92339db7a9c41e4ae
Leonardo Maldonado <leonardomso11@gmail.com>
2018-03-20 17:56:27 -0300

    Already configured (thank God)

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>WOO</div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('.container')
+);
