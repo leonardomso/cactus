adcd467a9102d3cf368a66cb3596462c4adbf4b3
Leonardo Maldonado <leonardomso11@gmail.com>
2018-03-21 13:53:52 -0300

    Redux working bi

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 class App extends React.Component {
     render() {
         return (
-            <div>WOO</div>
-        )
+            <Provider store={store}>
+                <div>Hello Redux!</div>
+            </Provider>
+        );
     }
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = (state = {}, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
