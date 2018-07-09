673f696fa5eb16af2d2ae70e66a38fd2a641c36a
leonardomso <leonardomso11@gmail.com>
2018-07-09 10:49:03 -0300

    Done tasks and doing tasks repositioned

--- src/components/TodoTasks.js
+++ src/components/TodoTasks.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react';
-import Task from './Task';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import Task from "./Task";
+import { connect } from "react-redux";
 
 class TodoTasks extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	render() {
-		console.log(this.props.tasks);
-		return (
-			<div className="task todo__tasks">
-				<h1 className="tasks__title">Todo Tasks</h1>
-				<div className="tasks__item">
-					<p>hahha</p>
-					<svg
-						className="tasks__icon tasks__check"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 512 512">
-						<path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
-					</svg>
-					<svg
-						className="tasks__icon tasks__cross"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 384 512">
-						<path d="M323.1 441l53.9-53.9c9.4-9.4 9.4-24.5 0-33.9L279.8 256l97.2-97.2c9.4-9.4 9.4-24.5 0-33.9L323.1 71c-9.4-9.4-24.5-9.4-33.9 0L192 168.2 94.8 71c-9.4-9.4-24.5-9.4-33.9 0L7 124.9c-9.4 9.4-9.4 24.5 0 33.9l97.2 97.2L7 353.2c-9.4 9.4-9.4 24.5 0 33.9L60.9 441c9.4 9.4 24.5 9.4 33.9 0l97.2-97.2 97.2 97.2c9.3 9.3 24.5 9.3 33.9 0z" />
-					</svg>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="task todo__tasks">
+                <h1 className="tasks__title">Todo Tasks</h1>
+                <div className="tasks__item">
+                    <p>hahha</p>
+                    <svg
+                        className="tasks__icon tasks__check"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                    >
+                        <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
+                    </svg>
+                    <svg
+                        className="tasks__icon tasks__cross"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 384 512"
+                    >
+                        <path d="M323.1 441l53.9-53.9c9.4-9.4 9.4-24.5 0-33.9L279.8 256l97.2-97.2c9.4-9.4 9.4-24.5 0-33.9L323.1 71c-9.4-9.4-24.5-9.4-33.9 0L192 168.2 94.8 71c-9.4-9.4-24.5-9.4-33.9 0L7 124.9c-9.4 9.4-9.4 24.5 0 33.9l97.2 97.2L7 353.2c-9.4 9.4-9.4 24.5 0 33.9L60.9 441c9.4 9.4 24.5 9.4 33.9 0l97.2-97.2 97.2 97.2c9.3 9.3 24.5 9.3 33.9 0z" />
+                    </svg>
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-	return {
-		tasks: state.tasks
-	};
+    return {
+        tasks: state.tasks
+    };
 };
 
 export default connect(mapStateToProps)(TodoTasks);
--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import AddTask from './AddTask';
-import Tasks from './Tasks';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import AddTask from "./AddTask";
+import Tasks from "./Tasks";
 
 const App = props => (
-	<div className="app">
-		<AddTask />
-		<Tasks />
-	</div>
+    <div className="app">
+        <AddTask />
+        <Tasks />
+    </div>
 );
 
 const mapStateToProps = state => {
-	return {
-		tasks: state.tasks
-	};
+    return {
+        tasks: state.tasks
+    };
 };
 
 export default connect(mapStateToProps)(App);
