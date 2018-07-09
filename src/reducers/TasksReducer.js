902735422b4e34af19a2a4bf17f2cd0970082b22
leonardomso <leonardomso11@gmail.com>
2018-07-09 14:10:29 -0300

    fixed, missing delete

--- src/reducers/TasksReducer.js
+++ src/reducers/TasksReducer.js
@@ -1,4 +1,4 @@
-import * as constants from "../../constants/";
+import * as constants from "../constants/";
 import uuid from "uuid";
 
 const initialState = {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import TasksReducer from "./Tasks/TasksReducer";
+import TasksReducer from "./TasksReducer";
 
 const rootReducer = combineReducers({
     tasks: TasksReducer
